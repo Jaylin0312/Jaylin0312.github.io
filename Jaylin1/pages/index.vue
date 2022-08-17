1902bdf301ce0880a68e8d20535d9b08c57cf420
Jaylin0312 <jieling.lee.2021@smu.edu.sg>
2022-08-17 19:05:22 +0800

    building ICE1

---
+++ Jaylin1/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id=bg class="p-4" style="height: 100vh">
+    <h1 class="text-center mb-5">Hello, I am Jaylin</h1>
+    <div class="section-about">
+      <div class="d-flex justify-content-center align-items-center">
+        <div class="col-2">
+          <img src="../assets/myProfile.jpg" width="100%" height="100%"/>
+        </div>
+        <div class="col-8">
+          I am Jie Ling, also known by my nickname Jaylin. I am a sophomore in
+          SMU studying Information System. I love meeting new people, having new
+          friends, and gaining new experience.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#bg {
+  background-color: cornsilk ;
+}
+
+</style>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
